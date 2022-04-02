1d04b920348e7ea5f15c65177970356ccc72fde3
zx5481 <zxcasd5481@gmail.com>
2022-04-02 23:51:20 +0900

    paraoh

--- c_study_2/C_Study_2.c
+++ c_study_2/C_Study_2.c
@@ -1,2 +1,33 @@
-#include <stdio>
+#include <stdio.h>
 
+void main() {
+
+	printf
+
+
+
+	/*int start = 0;
+
+	printf("피라미드를 쌓아라. (시작: 1, 종료: 0)\n");
+	scanf_s("%d", &start);
+
+	if(start == 1) {
+		int layer = 0;
+
+		printf("몇 층으로 쌓겠느냐?\n");
+		scanf_s("%d", &layer);
+
+		if(5 > layer || layer > 50) {
+			printf("범위 내에 층을 이야기하거라.\n");
+		}
+		else {
+			printf("%d 층\n", layer);
+			for (int i = 0; i < layer; i++) {
+				for (int k = 0; k < i + 1; k++) {
+					printf("*");
+				}
+				printf("\n");
+			}
+		}
+	}*/
+}
